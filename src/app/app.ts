a4a53f42898a947c6e55c457137113409649e271
bart <cieslabartek721@gmail.com>
2026-03-13 21:10:10 +0100

    Dodanie loadera w formie progress baru

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,8 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, PLATFORM_ID, signal } from '@angular/core';
+import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router, RouterOutlet } from '@angular/router';
 import { Navbar } from "./shared/components/navbar/navbar";
 import { Footer } from "./shared/components/footer/footer";
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +11,30 @@ import { Footer } from "./shared/components/footer/footer";
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('telcostruct-portfolio');
+  private router = inject(Router);
+  private platformId = inject(PLATFORM_ID); // Sprawdzamy czy to przeglądarka czy serwer
+  
+  isLoading = signal(false);
+
+  constructor() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        this.isLoading.set(true);
+      } else if (
+        event instanceof NavigationEnd || 
+        event instanceof NavigationCancel || 
+        event instanceof NavigationError
+      ) {
+        // Ponieważ po zakończeniu nawigacji chcemy wyłączyć loader w przeglądarce:
+        if (isPlatformBrowser(this.platformId)) {
+          setTimeout(() => {
+            this.isLoading.set(false);
+          }, 400);
+        } else {
+          // Na serwerze wyłączamy od razu, żeby nie blokować SSR
+          this.isLoading.set(false);
+        }
+      }
+    });
+  }
 }
