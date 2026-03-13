7e55d62af272199412077a794700ceb49e679d8a
bart <cieslabartek721@gmail.com>
2026-03-13 19:02:42 +0100

    Dodanie strony z błędnym adresem

--- src/app/features/error-page/error-page.ts
+++ src/app/features/error-page/error-page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { isPlatformServer } from '@angular/common';
+import { Component, Inject, Optional, PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'app-error-page',
@@ -7,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './error-page.scss',
 })
 export class ErrorPage {
-
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    @Optional() @Inject(Response) private response: Response
+  ) {
+    // Jeśli renderujemy na serwerze, ustawiamy status 404 dla SEO
+    if (isPlatformServer(this.platformId) && this.response) {
+      this.response.status;
+    }
+  }
 }
