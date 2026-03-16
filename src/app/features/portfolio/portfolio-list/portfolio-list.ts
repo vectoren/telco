5d023fcb9920816296aa34665f9526c6a831e583
vectoren <cieslabartek721@gmail.com>
2026-03-16 09:56:03 +0100

    Dodanie wyświetlania elementów portfolio

--- src/app/features/portfolio/portfolio-list/portfolio-list.ts
+++ src/app/features/portfolio/portfolio-list/portfolio-list.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { PortfolioCard } from "../../../shared/components/portfolio-card/portfolio-card";
+import { PORTFOLIO_DATA } from '../../../core/company-data';
 
 @Component({
   selector: 'app-portfolio-list',
-  imports: [],
+  imports: [PortfolioCard],
   templateUrl: './portfolio-list.html',
   styleUrl: './portfolio-list.scss',
 })
 export class PortfolioList {
+  projects = PORTFOLIO_DATA;
 
 }
