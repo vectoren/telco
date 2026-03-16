ccbcf2e056580e79606d14ad554364b3cd9506e6
vectoren <cieslabartek721@gmail.com>
2026-03-16 11:08:17 +0100

    Dodanie wyświetlania szczegółowego dla portfolio

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
@@ -8,6 +8,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes, withComponentInputBinding()), provideClientHydration(withEventReplay())
   ]
 };
--- src/app/features/portfolio/portfolio-details/portfolio-details.ts
+++ src/app/features/portfolio/portfolio-details/portfolio-details.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, computed, Input, signal } from '@angular/core';
+import { PORTFOLIO_DATA } from '../../../core/company-data';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-portfolio-details',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './portfolio-details.html',
   styleUrl: './portfolio-details.scss',
 })
 export class PortfolioDetails {
+  projectSlug = signal<string | null>(null);
+
+  @Input() set slug(value: string) { // Angular mapuje :slug z URL na ten input
+     this.projectSlug.set(value);
+  }
+
+  // Automatycznie wyliczamy dane projektu na podstawie sluga
+  project = computed(() => {
+    const currentSlug = this.projectSlug();
+    if (!currentSlug) return null;
+    
+    return PORTFOLIO_DATA.find(p => p.slug === currentSlug) || null;
+  });
 
 }
